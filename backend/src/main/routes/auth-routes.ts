529e1fa79041b2dc06c0a2b838e43c0cde55edc0
ArthurMaverick <arthuracs18@gmail.com>
2021-09-02 23:11:27 -0300

    fix: fixed some bugs

--- backend/src/main/routes/auth-routes.ts
+++ backend/src/main/routes/auth-routes.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express'
-import { uri } from '../config/env'
 
 export default (router: Router) => {
   router.get('/redirect', async (req, res) => {
-    res.redirect(uri.link)
+    res.redirect('https://discord.com/api/oauth2/authorize?client_id=881056837099716668&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fv1%2Fany&response_type=code&scope=identify')
   })
 }
